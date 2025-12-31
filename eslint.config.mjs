f7a8d30b4f3623c6da4ba8551749654df6e7134d
jadhavharshh <harshjadhavconnect@gmail.com>
2025-12-31 16:45:23 +0530

    fix: update ESLint configuration and package dependencies for improved linting and compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,21 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Ignore unused variables and parameters
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react/no-unescaped-entities': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-    }
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    // Ignore unused variables and parameters
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
   }
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
